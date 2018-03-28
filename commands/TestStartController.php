f8ef8b82db937a593d02709fbc770ad9ec4bfae2
rdbcache <rdbcache.s@gmail.com>
2018-03-28 08:53:28 -0400

    add database checking before test

--- commands/TestStartController.php
+++ commands/TestStartController.php
@@ -14,6 +14,36 @@ class TestStartController extends TestController
 {
     public function actionIndex()
     {
+        $tables_needed = array(
+            "rdbcache_kv_pair", "rdbcache_monitor", "rdbcache_client_test", "rdbcache_stopwatch", 
+            "departments", "employees", "dept_emp", "dept_manager", "titles", "salaries", 
+            "departments2", "employees2", "dept_emp2", "dept_manager2", "titles", "salaries2", 
+            "tb1", "tb2", "user_table");
+        try {
+            $conn = Yii::$app->db;
+            $tables = $conn->schema->getTableNames();
+            $missing_tables = "";
+            foreach ($tables_needed as $table_needed) {
+                $hasIt = false;
+                foreach($tables as $table) {
+                    if ($table == $table_needed) {
+                        $hasIt = true;
+                        break;
+                    }
+                }
+                if (!$hasIt) {
+                    $missing_tables .= " " . $table_needed;
+                }
+            }
+            if (!empty($missing_tables)) {
+                echo "mssing needed table(s): ".$missing_tables;
+                exit(1);
+            }
+        } catch (\yii\db\Exception $e) {
+            echo "failed to connect to database";
+            exit(1);
+        }
+
         echo $this->getTestStartAtId();
     }
 }
